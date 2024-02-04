8922107f7a28e53026dde9ad12fc3ebf9bfb0d5d
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2024-02-04 17:02:37 -0600

    fix: getTokenBalance

--- src/functions/getTokenBalance.tsx
+++ src/functions/getTokenBalance.tsx
@@ -1,17 +1,19 @@
 import Web3 from "web3";
 
-export const getTokenBalance = async (account, ABI, tokenAddress, chainId) => {
+export async function getTokenBalance (ABI, tokenAddress) {
   // web3
   const web3 = new Web3(window.ethereum);
-  const accounts = await web3.eth.getAccounts()
-  account = accounts[0]
+
+  // gets: account of the connected wallet (refresh)
+  const accounts = await web3.eth.getAccounts();
+  const selectedAccount = accounts[0];
 
   // [√] set: token contract
   const tokenContract = await new web3.eth.Contract(ABI, tokenAddress);
 
   // this is the payable amount to send
-  const balance = await tokenContract.methods.balanceOf(account).call();
-  // console.log("balance: %s", balance);
+  const balance = await tokenContract.methods.balanceOf(selectedAccount).call();
+  console.log("balance: %s", balance);
 
   return balance;
 };
